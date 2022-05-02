fc124529096edfef170098d2c039564255bc9bfb
MichaelL4438 <michael@lachut.com>
2022-05-02 08:13:40 -0400

    Modified setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='bs',
-    version='1.0.0',
+    version='1.0.17',
     description='A template engine for Make',
     url='http://github.com/michaell4438/bs',
     author='Michael Lachut',
     author_email='michael@lachut.com',
     license='GPL3',
-    packages=["."],
+    packages=["bs"],
+    include_package_data=True,
     entry_points=dict(
-        console_scripts=['bs=bs:exec']
+        console_scripts=['bs=bs.bs:main']
     )
 )
